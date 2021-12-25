b3562d789d4317779d8bab26b1399a764e2b76fc
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-26 00:48:59 +0100

    Router put group and put url

--- src/users/index.js
+++ src/users/index.js
@@ -26,5 +26,47 @@ userRouter
         next(error)
     }
 })
+.put('/group/:userId',async(req,res,next)=>{
+    try {
+        const newGroup={
+            groupName:req.body.name,
+            groupNumber:req.body.number,
+            groupColor:req.body.color,
+            urls:[]
+        }
+        const updatedUser=await UserModel.findByIdAndUpdate(
+            req.params.userId,
+            {$push:{groups:newGroup}},
+            {new:true}
+        )
+        if(updatedUser){
+            res.status(201).send(updatedUser)
+        }else{
+            res.status(404).send('USER NOT FOUND')
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+.put('/url/:userId/:groupName',async(req,res,next)=>{
+    try {
+        const newUrl={
+            urlName:req.body.name,
+            urlString:req.body.string
+        }
+        const updatedUser=await UserModel.findByIdAndUpdate(
+            req.params.userId,
+            {$push:{groups:{groupName:req.params.groupName}}},
+            {new:true}
+        )
+        if(updatedUser){
+            res.status(201).send(updatedUser)
+        }else{
+            res.status(404).send('USER NOT FOUND')
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default userRouter
