ef514278dd193298de029fcd66942b15dd5796f7
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-08 14:16:49 +0100

    group REST

--- src/groupsOfUrls/index.js
+++ src/groupsOfUrls/index.js
@@ -13,6 +13,37 @@ groupRouter
         next(error)
     }
 })
-.get
+.get('/:groupId',async(req,res,next)=>{
+    try {
+        const group=await GroupModel.findById(req.params.groupId)
+        res.status(200).send(group)
+    } catch (error) {
+        next(error)
+    }
+})
+.put('/:groupId',async(req,res,next)=>{
+    try {
+        const modifiedGroup=await GroupModel.findByIdAndUpdate(req.params.groupId,req.body,{new:true})
+        if(modifiedGroup){
+            res.status(200).send(modifiedGroup)
+        }else{
+            res.status(404).send(`GROUP ID ${req.params.groupId} NOT FOUND`)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+.delete('/:groupId',async(req,res,next)=>{
+    try {
+        const deletedGroup=await GroupModel.findByIdAndDelete(req.params.groupId)
+        if(deletedGroup){
+            res.status(200).send(`GROUP ID ${req.params.groupId} IS GONE`)
+        }else{
+            res.status(404).send(`GROUP ID ${req.params.groupId} NOT FOUND`)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default groupRouter
