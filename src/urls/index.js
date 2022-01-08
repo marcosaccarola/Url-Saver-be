9b9dccd87969dc46f9f071fc15a1d04994fac3a3
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-08 16:59:26 +0100

    url REST

--- src/urls/index.js
+++ src/urls/index.js
@@ -19,5 +19,37 @@ urlRouter
         next(error)
     }
 })
+.get('/:urlId',async(req,res,next)=>{
+    try {
+        const url=await UrlModel.findById(req.params.urlId)
+        res.status(200).send(url)
+    } catch (error) {
+        next(error)
+    }
+})
+.put('/:urlId',async(req,res,next)=>{
+    try {
+        const modifiedUrl=await UrlModel.findByIdAndUpdate(req.params.urlId,req.body,{new:true})
+        if(modifiedUrl){
+            res.status(200).send(modifiedUrl)
+        }else{
+            res.status(404).send(`URL ID ${req.params.urlId} NOT FOUND`)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+.delete('/:urlId',async(req,res,next)=>{
+    try {
+        const deletedUrl=await UrlModel.findByIdAndDelete(req.params.urlId)
+        if(deletedUrl){
+            res.status(200).send(`URL ID ${req.params.urlId} IS GONE`)
+        }else{
+            res.status(404).send(`URL ID ${req.params.urlId} NOT FOUND`)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default urlRouter
