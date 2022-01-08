1ee16ae380042920caf87796e96084cdf76b0d68
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-08 13:36:17 +0100

    renaming

--- src/users/index.js
+++ src/users/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import UserModel from './userSchema.js'
+import UserModel from './schema.js'
 
 const userRouter=express.Router()
 
@@ -26,49 +26,49 @@ userRouter
         next(error)
     }
 })
-.put('/:userId/group',async(req,res,next)=>{
-    try {
-        const newGroup={
-            groupName:req.body.name,
-            groupNumber:req.body.number,
-            groupColor:req.body.color,
-            urls:[]
-        }
-        const updatedUser=await UserModel.findByIdAndUpdate(
-            req.params.userId,
-            {$push:{groups:newGroup}},
-            {new:true}
-        )
-        if(updatedUser){
-            res.status(201).send(updatedUser)
-        }else{
-            res.status(404).send('USER NOT FOUND')
-        }
-    } catch (error) {
-        next(error)
-    }
-})
-.put('/:userId/:groupName/url',async(req,res,next)=>{
-    try {
-        const newUrl={
-            urlName:req.body.name,
-            urlString:req.body.string
-        }
+// .put('/:userId/group',async(req,res,next)=>{
+//     try {
+//         const newGroup={
+//             groupName:req.body.name,
+//             groupNumber:req.body.number,
+//             groupColor:req.body.color,
+//             urls:[]
+//         }
+//         const updatedUser=await UserModel.findByIdAndUpdate(
+//             req.params.userId,
+//             {$push:{groups:newGroup}},
+//             {new:true}
+//         )
+//         if(updatedUser){
+//             res.status(201).send(updatedUser)
+//         }else{
+//             res.status(404).send('USER NOT FOUND')
+//         }
+//     } catch (error) {
+//         next(error)
+//     }
+// })
+// .put('/:userId/:groupName/url',async(req,res,next)=>{
+//     try {
+//         const newUrl={
+//             urlName:req.body.name,
+//             urlString:req.body.string
+//         }
         
-        const user=await UserModel.findById(
-            req.params.userId
-            )
-        const targetGroup=user.groups.find(g=>g.groupName===req.params.groupName).urls.push(new)
-        const updatedUser=targetGroup.urls.push(newUrl)
+//         const user=await UserModel.findById(
+//             req.params.userId
+//             )
+//         const targetGroup=user.groups.find(g=>g.groupName===req.params.groupName).urls.push(new)
+//         const updatedUser=targetGroup.urls.push(newUrl)
             
-        if(user){
-            res.status(201).send(updatedUser)
-        }else{
-            res.status(404).send('USER NOT FOUND')
-        }
-    } catch (error) {
-        next(error)
-    }
-})
+//         if(user){
+//             res.status(201).send(updatedUser)
+//         }else{
+//             res.status(404).send('USER NOT FOUND')
+//         }
+//     } catch (error) {
+//         next(error)
+//     }
+// })
 
 export default userRouter
