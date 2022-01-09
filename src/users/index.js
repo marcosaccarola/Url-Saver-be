12b99c972fd968dc3060bc20a42e0daf2f4a7abd
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-09 18:38:49 +0100

    Update index.js

--- src/users/index.js
+++ src/users/index.js
@@ -26,49 +26,5 @@ userRouter
         next(error)
     }
 })
-// .put('/:userId/group',async(req,res,next)=>{
-//     try {
-//         const newGroup={
-//             groupName:req.body.name,
-//             groupNumber:req.body.number,
-//             groupColor:req.body.color,
-//             urls:[]
-//         }
-//         const updatedUser=await UserModel.findByIdAndUpdate(
-//             req.params.userId,
-//             {$push:{groups:newGroup}},
-//             {new:true}
-//         )
-//         if(updatedUser){
-//             res.status(201).send(updatedUser)
-//         }else{
-//             res.status(404).send('USER NOT FOUND')
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-// .put('/:userId/:groupName/url',async(req,res,next)=>{
-//     try {
-//         const newUrl={
-//             urlName:req.body.name,
-//             urlString:req.body.string
-//         }
-        
-//         const user=await UserModel.findById(
-//             req.params.userId
-//             )
-//         const targetGroup=user.groups.find(g=>g.groupName===req.params.groupName).urls.push(new)
-//         const updatedUser=targetGroup.urls.push(newUrl)
-            
-//         if(user){
-//             res.status(201).send(updatedUser)
-//         }else{
-//             res.status(404).send('USER NOT FOUND')
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// })
 
 export default userRouter
