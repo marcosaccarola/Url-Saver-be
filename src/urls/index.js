3a8dbe0bb836974c69940796575ebe9f7dae4b2b
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-11 17:46:34 +0100

    POST url

--- src/urls/index.js
+++ src/urls/index.js
@@ -1,19 +1,26 @@
 import express from 'express'
 import UrlModel from './schema.js'
 import GroupModel from '../groups/schema.js'
+import UserModel from '../users/schema.js'
 
 const urlRouter=express.Router()
 
 urlRouter
-.post('/:groupId',async(req,res,next)=>{
+.post('/:groupId/:userId',async(req,res,next)=>{
     try {
         const newUrl=new UrlModel(req.body)
         const savedUrl=await newUrl.save()
+        console.log('SAVED URL',savedUrl)
         const updatedGroup=await GroupModel.findByIdAndUpdate(
             req.params.groupId,
             {$push:{urls:savedUrl}},
             {new:true}
-        )
+            )
+            console.log('SAVED GROUP',updatedGroup)
+            const updatedUser=await UserModel.findById(
+                req.params.userId,
+                ).populate({path:'groups',select:'name urls'})
+                console.log('SAVED USER',updatedUser)
         res.status(201).send(updatedGroup)
     } catch (error) {
         next(error)
