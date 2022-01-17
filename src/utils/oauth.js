c55be7808c466803b911ff6709de647b55244519
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-17 12:34:17 +0100

    Google Strategy OAuth

---
+++ src/utils/oauth.js
@@ -0,0 +1,38 @@
+import passport from 'passport'
+import GoogleStrategy from 'passport-google-oauth20'
+import UserModel from '../users/schema.js'
+
+const GoogleStrategy=new GoogleStrategy(
+    {
+        clientID:process.env.GOOGLE_OAUTH_ID,
+        clientSecret:process.env.GOOGLE_OAUTH_SECRET,
+        callbackURL:process.env.GOOGLE_REDIRECT_URL
+    },
+    async(accessToken,refreshToken,profile,passportNext)=>{
+        console.log(profile)
+        const user=await UserModel.findOne({googleId:profile.id})
+        console.log(user)
+        if(user){
+            const token=await generateJwt({id:user._id.toString()})
+            console.log(user._id)
+            passportNext(null,{token})
+        }else{
+            const user=new UserModel(
+                {
+                    email:profile.emails[0].value,
+                    googleId:profile.id
+                }
+            )
+            await user.save()
+            console.log(user._id)
+            const token=await generateJwt({id:user._id.toString()})
+            passportNext(nulla,{token})
+        }
+    }
+)
+
+passport.serializeUser(function (userData,passportNext){
+    passportNext(null,userData)
+})
+
+export default googleStrategy
